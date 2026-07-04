51ef6c3bbece9ca809930d31a1073c097855d181
Chirag Kashyap <chiragk1412@gmail.com>
2026-07-04 14:15:08 +0530

    add: secret css

--- components/InputSection.tsx
+++ components/InputSection.tsx
@@ -71,7 +71,7 @@ const InputSection = ({
         {/* Secret Chance % */}
         <div className="bg-[#050911] w-full sm:w-1/3 h-17 rounded-[0.9rem] border border-[#212632] p-3 flex gap-3">
           <div className="box text-xl h-full w-11 shrink-0 border border-[#222b39] bg-[#131924] rounded-[0.6rem] flex justify-center items-center">
-            <span className="shimmer-text">???</span>
+            <span className="shimmer-text font-bold">???</span>
           </div>
           <div className="flex-1 h-full min-w-0">
             <h3 className="flex items-center gap-1 text-[#12ddf4] font-semibold font-mono text-[10px] uppercase tracking-[0.2em]">
@@ -172,7 +172,8 @@ const InputSection = ({
           </div>
           <div className="flex-1 h-full flex flex-col justify-center min-w-0">
             <h3 className="text-[#12ddf4] font-semibold font-mono text-[10px] uppercase tracking-[0.2em] truncate">
-              STAR (AUTO: {selectedSecret})
+              STAR (AUTO:
+              <span className="shimmer-text font-bold"> {selectedSecret})</span>
             </h3>
             <p className="mt-1 text-[1rem] leading-none font-semibold font-mono text-[#ffb634] [text-shadow:0_0_10px_rgba(255,182,52,0.2)] truncate">
               {secrets[selectedSecret.toLowerCase()].star} (
