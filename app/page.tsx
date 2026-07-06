bf35d8549de58a8094e333d83164b090480da2c2
Chirag Kashyap <chiragk1412@gmail.com>
2026-07-06 13:33:31 +0530

    style: increment max-width of page for wide screen users

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
   return (
     <div className="bg-[#0a0e19] min-h-screen w-full flex flex-col gap-5 py-7 px-4 sm:px-6 items-center">
-      <div className="w-full max-w-4xl flex flex-col gap-5">
+      <div className="w-full max-w-[62rem] flex flex-col gap-5">
         <InputSection
           selectedSecret={selectedSecret}
           setSelectedSecret={setSelectedSecret}
